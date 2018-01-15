23904cdd33f8183283793a76da49a475a1dd284c
Saša Prtvar <prtvar11@hotmail.com>
2018-01-15 10:39:42 -0700

    3

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ var express = require('express');
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
-var Mia and Sasa 
+var Mia and Sasa is a plus
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
